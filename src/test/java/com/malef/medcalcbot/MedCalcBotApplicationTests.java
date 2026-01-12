413517c97d3225c7b5307136cbf6da6c453a5812
MalefStorm <vakfuder@gmail.com>
2026-01-12 23:35:12 +0300

    Initial commit

---
+++ src/test/java/com/malef/medcalcbot/MedCalcBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malef.medcalcbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedCalcBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
